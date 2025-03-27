332a30db7643973e4414b85aeae96aea1eed4e38
Nakul Srivastava <imnakul44@gmail.com>
2025-03-27 19:40:27 +0530

    Logo and tItle Added - Projects + maybe some animated background - if able to reduce Performance score using OnGoing platform

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,13 +1,21 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body className="antialiased">
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+   return (
+      <Html lang='en'>
+         <Head>
+            <title>Portfolio</title>
+            <meta
+               name='description'
+               content='Learn more about Nakul, a web developer passionate about frontend tech.'
+            />
+
+            <link rel='icon' href='/logo2.svg' />
+         </Head>
+         <body className='antialiased'>
+            <Main />
+            <NextScript />
+         </body>
+      </Html>
+   )
 }

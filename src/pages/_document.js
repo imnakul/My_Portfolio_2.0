35fdf8ddf7198d96e74a30d47eff1fe636fdf54e
Nakul Srivastava <imnakul44@gmail.com>
2025-05-17 09:18:59 +0530

    logoupdated

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -9,10 +9,17 @@ export default function Document() {
                name='description'
                content='Learn more about Nakul, a web developer passionate about frontend tech.'
             />
-            <link rel="preconnect" href="https://fonts.googleapis.com" />
-            <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
-            <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700&family=Orbitron:wght@700;900&display=swap" rel="stylesheet" />
-            <link rel='icon' href='/logo2.svg' />
+            <link rel='preconnect' href='https://fonts.googleapis.com' />
+            <link
+               rel='preconnect'
+               href='https://fonts.gstatic.com'
+               crossOrigin='true'
+            />
+            <link
+               href='https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700&family=Orbitron:wght@700;900&display=swap'
+               rel='stylesheet'
+            />
+            <link rel='icon' href='/image.png' />
          </Head>
          <body className='antialiased'>
             <Main />
